1963565354202a47b8c46f7c714cf2cd290f93ec
joaquin <jm622@duke.edu>
2020-03-01 13:41:02 -0500

    set host to 0.0.0.0

--- main.py
+++ main.py
@@ -79,4 +79,4 @@ def describe_data():
 
 # Useful also to visualize locally
 if __name__ == "__main__":
-    app.run(host='0.0.0.0.', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
